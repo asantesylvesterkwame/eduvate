fb542f0901770230c8586c650d458d3154169985
asantesylvesterkwame <intelligentson17@gmail.com>
2023-03-11 01:23:05 +0000

    imported dotenv

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from "express"
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express()
 
-const mongoose =>{
+const connect =>{
     mongoose.connect("")
 }
 
