c90d764ffbafeacfa5c8dc80ab95fa734060fd45
asantesylvesterkwame <intelligentson17@gmail.com>
2023-03-11 01:20:07 +0000

    imported mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from "express"
+import mongoose from "mongoose";
 
 const app = express()
+
+const mongoose =>{
+    mongoose.connect("")
+}
 
 app.listen(8800,()=>{
     console.log("connected!");
