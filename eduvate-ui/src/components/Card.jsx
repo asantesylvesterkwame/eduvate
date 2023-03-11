cfcdd1bc40c17fe769f448848edd1d9ee89e5d37
asantesylvesterkwame <intelligentson17@gmail.com>
2023-03-11 02:35:09 +0000

    set the default images for all image points

--- eduvate-ui/src/components/Card.jsx
+++ eduvate-ui/src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import profilePic from "../img/A.jpg"
 
 const Container = styled.div`
   width: ${(props) => props.type !== "sm" && "360px"};
@@ -57,17 +58,17 @@ const Card = ({ type }) => {
       <Container type={type}>
         <Image
           type={type}
-          src="https://i9.ytimg.com/vi_webp/k3Vfj-e1Ma4/mqdefault.webp?v=6277c159&sqp=CIjm8JUG&rs=AOn4CLDeKmf_vlMC1q9RBEZu-XQApzm6sA"
+          src={profilePic}
         />
         <Details type={type}>
           <ChannelImage
             type={type}
-            src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo"
+            src={profilePic}
           />
           <Texts>
             <Title>Test Video</Title>
-            <ChannelName>Lama Dev</ChannelName>
-            <Info>660,908 views • 1 day ago</Info>
+            <ChannelName>Amplified</ChannelName>
+            <Info>1M attendances • 1 day ago</Info>
           </Texts>
         </Details>
       </Container>
