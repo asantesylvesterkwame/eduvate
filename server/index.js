f089387597f409c4d1df2eabe04536526fcd83d2
asantesylvesterkwame <intelligentson17@gmail.com>
2023-03-11 01:29:13 +0000

    wrote a test to import an env file to make sure it has been successfully done

--- server/index.js
+++ server/index.js
@@ -4,9 +4,14 @@ import dotenv from "dotenv";
 
 const app = express()
 
-const connect =>{
-    mongoose.connect("")
-}
+const connect = () => {
+    mongoose.connect(process.env.MONGO).then(()=>{
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        throw err;
+    });
+};
 
 app.listen(8800,()=>{
     console.log("connected!");
