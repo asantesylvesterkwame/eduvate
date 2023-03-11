8a9ffdb71d9925546d903121a202f1dbbaac16e4
asantesylvesterkwame <intelligentson17@gmail.com>
2023-03-11 01:48:02 +0000

    created a schema for teacher users so I can define my data for mongo DB

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,31 @@
 import mongoose from "mongoose";
 
-const 
+const UserSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required:true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required:true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required:true,
+    },
+    img:{
+        type: String,
+    },
+    students:{
+        type: Number,
+        default:0
+    },
+    enrolledStudents:{
+        type: [String],
+    },
+    
+},{timestamps: true});
+
+export default mongoose.model("User", UserSchema)
